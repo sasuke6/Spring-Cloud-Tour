86a96fd674f0880c9bddb85816ed59631d234563
Hsu <xusasuke6@gmail.com>
2018-04-27 00:28:37 +0800

    [5]Spring Cloud Feign

---
+++ fegin-service/src/main/java/com/hsucy/services/HelloService.java
@@ -0,0 +1,12 @@
+package com.hsucy.services;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@FeignClient("hello-service")
+public interface HelloService {
+
+    @RequestMapping("/hello")
+    String hello();
+}
