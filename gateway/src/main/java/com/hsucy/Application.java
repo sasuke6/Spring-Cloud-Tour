a1c5696f694b0ab298c3a1f5c63de3a3c0d94cb6
Hsu <xusasuke6@gmail.com>
2018-05-04 23:53:22 +0800

    [6]Spring Cloud Zuul2

--- gateway/src/main/java/com/hsucy/Application.java
+++ gateway/src/main/java/com/hsucy/Application.java
@@ -1,9 +1,11 @@
 package com.hsucy;
 
+import Filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
@@ -11,5 +13,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
 	}
 }
